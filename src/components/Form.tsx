5a094b891d7f38ba3f204a7955cbe5047a36e2b0
Taha <with.tatsuhiko@gmail.com>
2025-11-21 18:16:22 +0100

    Create Form.tsx

---
+++ src/components/Form.tsx
@@ -0,0 +1,10 @@
+// TODO A FLexible form to encompass both login and signup
+// @param type {login, signup}
+// @param api endpoint
+// @param schema validation
+
+export default function Form() {
+    return (
+        <div>Form</div>
+    )
+}
